658dde0d3f45e994b79c6f409b6b1fb6cd3c0ad5
lucaArchidiacono <mail@archiluc.ch>
2025-05-19 10:35:27 +0200

    add xcodebuild.nvim plugin to lazy.nvim

--- init.lua
+++ init.lua
@@ -358,6 +358,22 @@ require('lazy').setup({
   --
   -- Use the `dependencies` key to specify the dependencies of a particular plugin
 
+  {
+    'wojciech-kulik/xcodebuild.nvim',
+    dependencies = {
+      'nvim-telescope/telescope.nvim',
+      'MunifTanjim/nui.nvim',
+      'folke/snacks.nvim', -- (optional) to show previews
+      'nvim-tree/nvim-tree.lua', -- (optional) to manage project files
+      'stevearc/oil.nvim', -- (optional) to manage project files
+      'nvim-treesitter/nvim-treesitter', -- (optional) for Quick tests support (required Swift parser)
+    },
+    config = function()
+      require('xcodebuild').setup {
+        -- put some options here or leave it empty to use default settings
+      }
+    end,
+  },
   { -- Fuzzy Finder (files, lsp, etc)
     'nvim-telescope/telescope.nvim',
     event = 'VimEnter',
